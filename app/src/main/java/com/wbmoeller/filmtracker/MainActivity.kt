7aecc2f1d57be7b5cde3fbe11dba9ef95481c1bc
Bill <wbmoeller@gmail.com>
2020-03-06 20:32:04 -0800

    Still trying to get auth to work (keeps failing on expired api token)

--- app/src/main/java/com/wbmoeller/filmtracker/MainActivity.kt
+++ app/src/main/java/com/wbmoeller/filmtracker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.wbmoeller.filmtracker
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -11,6 +14,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.setupWithNavController
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.wbmoeller.filmtracker.databinding.ActivityMainBinding
 
@@ -33,7 +37,22 @@ class MainActivity : AppCompatActivity() {
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                    .setAction("Action") {
+                        Log.d("MainActivity", "current user was null, showing auth")
+                        val providers = arrayListOf(
+//                            AuthUI.IdpConfig.GoogleBuilder().build(),
+//                            AuthUI.IdpConfig.PhoneBuilder().build())
+                            AuthUI.IdpConfig.EmailBuilder().build())
+
+                        startActivityForResult(
+                            AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(providers)
+                                .build(),
+                            AUTH_REQUEST_CODE
+                        )
+
+                    }.show()
         }
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
@@ -49,15 +68,6 @@ class MainActivity : AppCompatActivity() {
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
         if (currentUser == null) {
-            startActivityForResult(
-                AuthUI.getInstance()
-                    .createSignInIntentBuilder()
-//                    .setTheme(R.style.FirebaseSignInTheme)
-                    .setAvailableProviders(arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build(), AuthUI.IdpConfig.EmailBuilder().build()))
-                    .build(),
-                0
-            )
-            // show the signup activity
         }
     }
 
@@ -70,5 +80,38 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d("MainActivity", "auth result $requestCode $resultCode")
+
+        if (requestCode == AUTH_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                Log.d("MainActivity", "Finished auth with user $user")
+                // ...
+            } else {
+                Log.d("MainActivity", "failed auth with error ${response?.error?.message}")
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
+    fun signOut() {
+        AuthUI.getInstance().signOut(this).addOnCompleteListener {
+            FirebaseAuth.getInstance().signOut()
+            finish()
+        }
+    }
+
+    companion object {
+        private const val AUTH_REQUEST_CODE = 5678
     }
 }
